39cbd3f4d37ccc16fcf6df38a19efd25bc2963c7
dartchemist <mashina1991@abv.bg>
2015-09-12 21:27:44 +0300

    1. Forgot to add with last commit

---
+++ WindowsPhoneClient.UI/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsPhoneClient.ServiceConsumer;
+using WindowsPhoneClient.UI.Infrastrucure;
+
+namespace WindowsPhoneClient.UI.ViewModels
+{
+    public class HomePageViewModel : BindingBase
+    {
+        private readonly IGetPartnersInformationService _getPartnersInformationService;
+
+        public HomePageViewModel(IGetPartnersInformationService getPartnersInformationService)
+        {
+            _getPartnersInformationService = getPartnersInformationService;
+        }
+
+        public void GetPartnersInformation()
+        {
+            _getPartnersInformationService.GetPartnersInformation();
+        }
+    }
+}
